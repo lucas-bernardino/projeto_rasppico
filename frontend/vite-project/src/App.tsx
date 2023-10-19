cec88d53fadeb745e8f3a47152d7963fe50629d8
Lucas <lucasgber@gmail.com>
2023-10-19 08:24:40 -0300

    testando a camera

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Sensor from '../components/Sensor'
+import CustomWebcam  from '../components/CustomWebcam'
+import WebcamTest from '../components/WebcamTest'
 
 function App() {
 
   return (
     <div className="App">
-      <Sensor></Sensor>
+      <div className="sensor-div">
+        <Sensor />
+      </div>
+      <div className="webcam-div">
+        <WebcamTest />
+      </div>
     </div>
   )
 }
