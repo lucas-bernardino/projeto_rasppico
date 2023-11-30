e93c4cda8e1d7b42172eac87a0ee347a76a93140
lucas-bernardino <122226174+lucas-bernardino@users.noreply.github.com>
2023-11-30 13:13:29 -0300

    Adicionei arquivos da rasp no modo online

---
+++ codigo_rasppi4/com_internet/main.py
@@ -0,0 +1,234 @@
+import time
+import threading
+import serial
+import binascii
+import RPi.GPIO as GPIO
+import datetime
+import requests
+import subprocess
+import multiprocessing
+from handle_sensors_module import *
+
+
+time.sleep(20)
+
+
+
+
+sensor_gps = serial.Serial("/dev/serial0", 9600, timeout=1)
+sensor_angulo = serial.Serial("/dev/ttyAMA2", 9600, timeout=11)
+data_sensors = ""
+
+def get_api_route():
+    command = [ "python3", "aux_email.py"]
+    subprocess.run(command)
+    with open("rotaapi.txt", "r") as file_:
+        return str(file_.readline())
+
+
+
+#process = multiprocessing.Process(target=record_video)
+#process.start()
+
+# UnicodeDecodeError: 'utf-8' codec can't decode byte 0xb2 in position 0: invalid start byte
+# corrigir esse erro. provavelmente pode colocar um try pq so acontece na primeira vez 
+# que aperta o botao
+
+# tem que arrumar o erro que na primeria vez que começa a rodar aidna nao existe o arquivo 
+# number.txt, dai ele joga um erro mas ao inves de so criar o arquivo e continuar o programa
+# parece que ele roda o erro e trava ali mesmo. solucionar isso
+# o erro: ERRO LINHA 29:  [Errno 2] No such file or directory: 'number.txt'
+
+BUTTON_GPIO = 26
+contador = 0
+interrupt_flag = False
+check_bug = True
+dados_package = {}
+contador_botao = 0
+flag_button_collection = False
+
+
+
+session = requests.Session()
+
+ROTA_API = get_api_route().rstrip()
+
+def button_pressed_callback(channel):
+    global interrupt_flag, data_sensors, contador_botao, flag_button_collection
+    interrupt_flag = not interrupt_flag
+    print("Button pressed")
+    current_time = datetime.datetime.now()
+    print(current_time)
+    print(f"Contador: {contador}")
+    data_sensors = ""
+    if not flag_button_collection:
+        new_collection = session.post(ROTA_API + "/button_pressed", json = {"contador": contador_botao})
+        print("Resposta button pressed: ", new_collection.text)
+        contador_botao+=1
+    flag_button_collection = not flag_button_collection
+    
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=button_pressed_callback, bouncetime=1000)
+
+def check_bug_timer():
+    global interrupt_flag, check_bug
+    if check_bug:
+        #Cancelar as duas threads?
+        interrupt_flag = False
+        time.sleep(0.5)
+        interrupt_flag = True
+
+
+def core0_thread():
+    global run_core_1
+    global data_sensors
+    global cont
+    global check_bug
+
+    while interrupt_flag:
+        sensor_a = sensor_gps.read(2)
+        if sensor_a:
+            if binascii.hexlify(sensor_a).decode('utf-8') == "5551" :
+                
+                    check_bug = False
+                    if not interrupt_flag:
+                        data_sensors = ""
+                        sensor_a = ""
+                        break
+                            
+                    data_worthy = "5551" + binascii.hexlify(sensor_gps.read(86)).decode('utf-8')
+                            
+                    data_sensors = ""
+                    data_sensors += data_worthy
+
+    
+    data_worthy = ""
+    data_sensors = ""
+
+
+def core1_thread():
+    global run_core_1
+    global data_sensors
+    global contador
+    global dados_package
+
+    while interrupt_flag:
+        sensor_b = sensor_angulo.readline()
+        if sensor_b:
+            sensor_b_decoded = sensor_b.decode('utf-8').replace("Angle:", "").rstrip()
+
+
+            if len(data_sensors) == 176:
+
+                if not interrupt_flag:
+                    data_sensors = ""
+                    sensor_b_decoded = ""
+                    break
+
+                #print("LEITURA HALL: ", hall.value()
+                
+                data_sensors += sensor_b_decoded
+
+                if len(data_sensors) > 20:
+                    arquivo.write(data_sensors + "\n")
+                    #print(f"DADOS COMBINADOS: {data_sensors}, horario: {datetime.datetime.now()}, cont: {contador}. Tam data sensors: {len(data_sensors)}")
+                    
+                    acel_x, acel_y, acel_z, temp = handleSensor1(data_sensors[0:22])
+                    vel_x, vel_y, vel_z = handleSensor2(data_sensors[22:44])
+                    roll, pitch, yaw = handleSensor3(data_sensors[44:66])
+                    mag_x, mag_y, mag_z = handleSensor4(data_sensors[66:88])
+                    air_press, altitude = handleSensor5(data_sensors[88:110])
+                    longitude, latitude = handleSensor6(data_sensors[110:132])
+                    velocidade_gps = handleSensor7(data_sensors[132:154])
+                    angle = data_sensors[176:]
+
+                    dados_package = {
+                    "id": contador,
+                    "acel_x": acel_x,
+                    "acel_y": acel_y,
+                    "acel_z": acel_z,
+                    "vel_x": vel_x,
+                    "vel_y": vel_y,
+                    "vel_z": vel_z,
+                    "roll": roll,
+                    "pitch": pitch,
+                    "yaw": yaw,
+                    "mag_x": mag_x,
+                    "mag_y": mag_y,
+                    "mag_z": mag_z,
+                    "temp": temp,
+                    "esterc": angle,
+                    "rot": "999",
+                    "veloc": velocidade_gps,
+                    "long": longitude,
+                    "lat": latitude,
+                    "press_ar": air_press,
+                    "altitude": altitude,
+                    }
+                    post_data = session.post(ROTA_API + "/enviar", json=dados_package)
+                    # #print(f"\nResposta: {req.text}\n")  
+                    print(dados_package)
+
+                    contador +=1
+                    
+                data_sensors = ""
+
+    
+    sensor_b_decoded = ""
+    data_sensors = ""
+
+
+while True:
+    if interrupt_flag:
+        print("entrei na interrupcao")
+        data_sensors = ""
+
+        sensor_gps.flushInput()
+        sensor_gps.flushOutput()
+        sensor_angulo.flushInput()
+        sensor_angulo.flushOutput()
+
+
+        try:
+            read_number = open('number.txt', 'r')
+            last_number = int(read_number.read())
+            arquivo = open(f"dados{str(last_number + 1)}.txt", "a")
+            write_number = open('number.txt', 'w')
+            write_number.write(str(last_number + 1))
+            read_number.close()
+            write_number.close()
+        except Exception as e:
+            print("ERRO LINHA 29: ", e)
+            arquivo = open("dados0.txt", "a")
+            write_number = open('number.txt', 'a')
+            write_number.write('0')
+            write_number.close()
+            pass
+
+        timer_thread = threading.Timer(1.5, check_bug_timer)  
+        check_bug = True  
+        timer_thread.start()
+        
+        thread1 = threading.Thread(target=core1_thread)
+        thread1.start()
+
+        
+        core0_thread()
+        
+        data_sensors = ""
+        print("apos as threads")
+
+        sensor_gps.flushInput()
+        sensor_gps.flushOutput()
+        sensor_angulo.flushInput()
+        sensor_angulo.flushOutput()
+
+        arquivo.close()
+
+
+
+# erro caso a rasp fique sem internet: OSError: [Errno 101] Network is unreachable
+
