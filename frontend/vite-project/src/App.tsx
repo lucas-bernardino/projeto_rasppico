63b9ece6c8e5aa295b81f59d5950510473f6310d
lucas-bernardino <lucasgber@gmail.com>
2024-02-16 19:07:33 -0300

    Refactor code and add documentation on backend routes

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -1,13 +1,8 @@
-import './App.css'
-import Sensor from '../components/Sensor'
-import WebcamUser from '../components/WebcamUser';
-import Action from '../components/Action';
-import Chart from '../components/Chart';
-import SendChart from '../components/SendChart';
-
+import "./App.css";
+import Sensor from "../components/Sensor";
+import Action from "../components/Action";
 
 function App() {
-
   return (
     <div className="app">
       <div className="sensor-div">
@@ -17,11 +12,8 @@ function App() {
         <Action />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
 
-// https://github.com/samuelweckstrom/react-record-webcam
-
-// https://stackoverflow.com/questions/67828731/react-webcam-recording-replay-re-recording-storing
